b1951cb021f0227633eff8723b2e4638e58f8543
sajidmd99 <sajidmohammad94@gmail.com>
2021-08-05 12:32:46 +0530

    supporting different delimiters

--- src/main/java/com/sajid/IncubyteStringCalculator.java
+++ src/main/java/com/sajid/IncubyteStringCalculator.java
@@ -3,15 +3,32 @@ package com.sajid;
 public class IncubyteStringCalculator {
     public static int add(String numbers) {
         //for empty string
-        if(numbers.isEmpty())
-            return 0;
-        // for multiple numbers with commas and new lines
-        String[] arrOfNumbers = numbers.split(",|\n");
+        if(numbers.isEmpty()) return 0;
+
+        // for custom delimeter
+        String[] arrOfNumbers;
+        if(numbers.startsWith("//")) {
+            arrOfNumbers = splitByCustomDelimeter(numbers);
+        }
+        else {
+            arrOfNumbers = splitByLinesAndCommas(numbers);
+        }
         int sum = 0;
         for(String num: arrOfNumbers) {
             sum += Integer.parseInt(num);
         }
 
         return sum;
+    }
+
+    public static String[] splitByLinesAndCommas(String numbers) {
+        return numbers.split(",|\n");
+    }
+
+    public static String[] splitByCustomDelimeter(String numbers) {
+        String delimeter = "" + numbers.charAt(2);
+        String[] numArray = numbers.split("\n");
+
+        return numArray[1].split(delimeter);
     }
 }
